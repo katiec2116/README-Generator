5eb0af1aa8d988ee67fa714af25d10018f165880
katiec <katiecampbell21@outlook.com>
2020-09-30 19:25:19 -0400

    created dynamic badges

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,23 +1,9 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  let license;
-if (data.license == "MIT"){
-  license = "[![GitHub license](https://img.shields.io/github/license/Naereen/StrapDown.js.svg)](https://github.com/Naereen/StrapDown.js/blob/master/LICENSE)"
-}
-else if (data.license == "APACHE"){
-  license = "[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)"
-}
-else if ( data.license == "GNU GPL V3"){
-  license = "[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)"
-}
-else{
-  license = "[![License](https://img.shields.io/badge/License-EPL%201.0-red.svg)](https://opensource.org/licenses/EPL-1.0)"
-}
   return `# ${data.title}
   
 
-  
-  ${license}
+  ![GitHub license](https://img.shields.io/badge/license-${data.license}-green.svg)
 
 
 
