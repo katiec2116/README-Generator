3d14fa68fc6fec28aa46d00567e185030c76093e
katiec <katiecampbell21@outlook.com>
2020-09-30 14:59:27 -0400

    added questions

--- index.js
+++ index.js
@@ -3,4 +3,55 @@ const fs = require("fs");
 const generateMarkdown = require("./utils/generateMarkdown")
 
 
+inquirer.prompt([
+    {
+        type: "input",
+        message: "What is your github username?",
+        name: "github",
+    },
+    {
+        type: "input",
+        message: "What is your email?",
+        name: "email",
+    },
+    {
+        type: "input",
+        message: "What is the name of your project?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Write a short description",
+        name: "description",
+    },
+    {
+        type: "list",
+        message: "What kind of license does it have?",
+        name: "license",
+        choices: ['MIT',
+            "APACHE",
+            "GPL V2",
+            "other one"]
+    },
+    {
+        type: "input",
+        message: "What command should be run to install dependencies?",
+        name: "install",
+    },
+    {
+        type: "input",
+        message: "What command should be run to run tests?",
+        name: "tests",
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about using the repo?",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "What does the user need to know about contributing?",
+        name: "contribute",
+    }
+])
 
